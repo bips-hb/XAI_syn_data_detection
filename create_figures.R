98a3cd166c742d6078da77a426bbf327bf44db9a
nkoenen <niklas.koenen@gmail.com>
2025-02-25 07:29:12 +0000

    update results and figures

--- create_figures.R
+++ create_figures.R
@@ -2,10 +2,90 @@
 #                 Create all Figures for the manuscript
 ################################################################################
 library(ggplot2)
-
+library(patchwork)
+library(geomtextpath)
+library(rlang)
+library(data.table)
 
 # Set theme
-theme_set(theme_minimal()) # <- TODO
+theme_set(theme_minimal(base_size = 15))
+
+# Create folder for plots
+dir.create("figures", showWarnings = FALSE)
+
+################################################################################
+#                           MODEL PERFORMANCE
+#
+#       Show the performance of the models on the different datasets,
+#       synthesizers, model types and runs.
+################################################################################
+res_perf <- data.table(readRDS("./results/model_performance/model_performance.rds"))
+
+# Model Performance Examples ---------------------------------------------------
+df <- res_perf[
+   ((dataset == "adult_complete" & syn_name == "TabSyn") |
+    (dataset == "nursery" & syn_name == "CTGAN")) & model_name == "xgboost"]
+ggplot(df, aes(x = dataset, y = value, fill = metric)) +
+  geom_boxplot() +
+  geom_texthline(yintercept = 0.5, linetype = "dashed", label = "Random guessing") +
+  facet_grid(cols = vars(train), scales = "free") +
+  scale_fill_brewer(palette = "Dark2") +
+  labs(x = "Dataset", fill = "Metric", y = NULL) +
+  theme(legend.position = "top")
+
+ggsave("figures/model_performance_examples.pdf", width = 8, height = 5)
+
+
+# Whole model performance ------------------------------------------------------
+
+# Plot for correct and incorrect predictions
+thres1 <- 0.6
+thres2 <- 0.8
+df <- res_perf[metric == "Accuracy" & run <= 5, 
+              .(correct = sum(value > thres2), 
+                middle = sum(value > thres1 & value <= thres2),
+                incorrect = sum(value <= thres1)), 
+              by = c("syn_name", "model_name", "train")]
+df <- melt(df, id.vars = c("syn_name", "model_name", "train"))
+
+res_fill <- df[, .(value = sum(value)), by = c("syn_name", "train", "model_name")]
+max_sum <- res_fill[, max(value)]
+res_fill <- res_fill[value != max_sum, ]
+res_fill$variable <- "NA"
+res_fill$value <- max_sum - res_fill$value
+df <- rbind(df, res_fill)
+df$syn_name <- factor(df$syn_name, 
+                      levels = rev(c("TabSyn", "CTGAN", "TVAE", "CTAB-GAN+", "ARF", "synthpop")))
+
+df$variable <- factor(df$variable, levels = c("correct", "middle", "incorrect", "NA"),
+                       labels = c(paste0("Above ", thres2 * 100, "%"), 
+                                  paste0("Between ", thres1 * 100, "% and ", thres2 * 100, "%"),
+                                  paste0("Below ", thres1 * 100, "%"), "NA"))
+
+# Selection
+ggplot(df[train == "test data"], 
+       aes(x = value, y = syn_name, fill = variable)) +
+  geom_bar(stat = "identity", position = "stack", width = 0.9) +
+  geom_text(aes(label = ifelse(value > 10, value, "")), position = position_stack(vjust = 0.5)) +
+  scale_fill_manual(values = c("darkred", "darkorange", "darkgreen", "gray")) +
+  scale_x_continuous(expand = c(0,0)) +
+  facet_grid(cols = vars(model_name), rows = vars(train), scales = "free") +
+  labs(x = "Frequency", y = "Synthesizer", fill = "Accuracy") +
+  theme(legend.position = "top",
+        plot.margin = margin(0,0,0,0))
+ggsave("figures/model_performance_selection.pdf", width = 8, height = 4)
+
+# Full plot
+ggplot(df, aes(x = value, y = syn_name, fill = variable)) +
+  geom_bar(stat = "identity", position = "stack", width = 0.9) +
+  geom_text(aes(label = ifelse(value > 10, value, "")), position = position_stack(vjust = 0.5)) +
+  scale_fill_manual(values = c("darkred", "darkorange", "darkgreen", "gray")) +
+  scale_x_continuous(expand = c(0,0)) +
+  facet_grid(cols = vars(model_name), rows = vars(train), scales = "free") +
+  labs(x = "Frequency", y = "Synthesizer", fill = "Accuracy") +
+  theme(legend.position = "top",
+        plot.margin = margin(0,0,0,0))
+ggsave("figures/model_performance_full.pdf", width = 12, height = 7)
 
 ################################################################################
 #                           RESEARCH QUESTION 1 (Q1)
@@ -13,3 +93,100 @@ theme_set(theme_minimal()) # <- TODO
 #       Which features and feature interactions were most challenging 
 #                       for the generative model?
 ################################################################################
+res_q1 <- data.table(rbind(
+  readRDS("./results/Q1/feature_importance.rds"),
+  readRDS("./results/Q1/cond_feature_importance.rds")
+))
+
+# Plot for adult_complete ------------------------------------------------------
+df <- res_q1[dataset_name == "adult_complete", ]
+df$type <- ifelse(df$method == "Model (impurity)", "model-specific", "model-agnostic")
+
+ggplot(df, aes(y = feature, x = value, fill = method)) +
+  geom_boxplot(position = "dodge") +
+  geom_vline(xintercept = 0, linetype = "dashed") +
+  facet_grid(cols = vars(type), scales = "free_x") +
+  scale_fill_brewer(palette = "Dark2") +
+  theme(legend.position = "top") +
+  labs(x = "Importance", y = "Feature", fill = NULL)
+
+ggsave("figures/Q1_adult_complete.pdf", width = 9, height = 5)
+
+# Plot for nursery -------------------------------------------------------------
+df <- res_q1[dataset_name == "nursery", ]
+df$type <- ifelse(df$method == "Model (impurity)", "model-specific", "model-agnostic")
+
+ggplot(df, aes(y = feature, x = value, fill = method)) +
+  geom_boxplot(position = "dodge") +
+  geom_vline(xintercept = 0, linetype = "dashed") +
+  facet_grid(cols = vars(type), scales = "free_x") +
+  scale_fill_brewer(palette = "Dark2") +
+  theme(legend.position = "top") +
+  labs(x = "Importance", y = "Feature", fill = NULL)
+
+ggsave("figures/Q1_nursery.pdf", width = 9, height = 5)
+
+
+################################################################################
+#                         RESEARCH QUESTION 2 (Q2)
+#
+#             TODO: Add description of the research question
+################################################################################
+res_q2 <- data.table(readRDS("./results/Q2/feat_effects.rds"))
+res_q2_rugs <- data.table(readRDS("./results/Q2/feat_effects_rugs.rds"))
+
+if (!dir.exists("figures/Q2")) dir.create("figures/Q2")
+
+# Plot for adult_complete ------------------------------------------------------
+df <- res_q2[dataset_name == "adult_complete", ]
+df_rug <- res_q2_rugs[dataset_name == "adult_complete", ]
+tmp <- lapply(unique(df$feature), function(feat) {
+  df_ice <- df[feature == feat & method == "ice", ]
+  df_pdp <- df[feature == feat & method == "pdp" & real != "both", ]
+  df_pdp_full <- df[feature == feat & method == "pdp", ]
+  df_rug_feat <- df_rug[variable == feat, ]
+  
+  if (all(df_ice$feat_type == "numeric")) {
+    df_rug_feat$gridpoint <- df_rug_feat$value
+    p <- ggplot(mapping = aes(x = as.numeric(as.character(gridpoint)))) +
+      geom_line(data = df_ice, aes(group = id, color = real, y = value), alpha = 0.5, linewidth = 0.3) +
+      geom_line(aes(group = real, y = value), data = df_pdp_full, color = "gray25", linewidth = 1) +
+      geom_line(aes(color = real, y = value), data = df_pdp, linewidth = 1, linetype = "dashed") +
+      geom_rug(data = df_rug_feat, aes(color = real), sides = "b", alpha = 0.5) +
+      labs(x = "Feature value", y = "Prediction", color = NULL) +
+      theme(legend.position = "top")
+    
+    if (feat %in% c("capital_gain", "capital_loss")) {
+      p <- p + scale_x_continuous(transform = "log2")
+    }
+  } else {
+    df_rug_feat <- df_rug_feat[, .(count = .N / nrow(df_rug_feat)), by = c("real", "value")]
+    p <- ggplot(df_ice, aes(y = gridpoint, x = value, fill = real)) +
+      geom_bar(stat = "identity", data = df_rug_feat, aes(y = value, x = count, fill = real), alpha = 0.5, inherit.aes = FALSE) +
+      labs(x = "Prediction", y = "Feature value", fill = NULL) +
+      geom_boxplot() + 
+      theme(legend.position = "top")
+  }
+  
+  ggsave(paste0("figures/Q2/Q2_adult_complete_", feat, ".pdf"), p, width = 8, height = 5)
+})
+
+res <- res_q2[dataset_name == "adult_complete" & method %in% c("pdp", "ale") & feature == "age"]
+res$value <- ifelse(res$method == "pdp", res$value - 0.5, res$value)
+ggplot(res) +
+  geom_line(aes(x = as.numeric(as.character(gridpoint)), y = value, color = method, group = method), alpha = 0.5)
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
--- run_feat_effects.R
+++ run_feat_effects.R
@@ -15,20 +15,23 @@ library(data.table)
 library(doParallel)
 library(parallel)
 library(ggplot2)
+
 cli_div(theme = list(span.emph = list(color = "#3c77b9")))
 
 # Set seed for reproducibility
 set.seed(42)
 
 # Manage number of cores and RAM
-n_threads <- 100L
+n_threads <- 200L
 
 options(future.globals.maxSize = 25000 * 1024^2)
 Sys.setenv(R_RANGER_NUM_THREADS = n_threads)
 Sys.setenv(OMP_THREAD_LIMIT = n_threads)
 
 # Global arguments for the PFI method
-NUM_SAMPLES <- 2000 # Number of samples for the calculation
+NUM_SAMPLES <- 1000
+NUM_SAMPLES_ICE <- 100
+NUM_GRID_POINTS <- 200
 
 # Define global arguments
 filter_df <- data.table(
@@ -100,59 +103,105 @@ res <- lapply(seq_len(nrow(df)), function(i) {
   pred_fun <- get_predict_fun(df$model_name[i])
   
   # Get sample of data
-  df_test <- data[sample(1:nrow(data), NUM_SAMPLES), ]
+  num_samples <- min(NUM_SAMPLES, nrow(data))
+  num_samples_ice <- min(NUM_SAMPLES_ICE, num_samples)
+  df_test <- data[sample(1:nrow(data), num_samples), ]
   
-  # Create predictor object
+  # Get predictions
+  preds <- pred_fun(model, df_test[, -ncol(df_test)])
+  preds <- data.table(preds, id = seq_len(nrow(df_test)))
+  preds <- preds[order(-rank(preds)), ]
+  
+  # Create predictor
   predictor <- Predictor$new(model, data = df_test, y = "real", class = "Real",
                              predict.function = pred_fun)
   
-  # ICE + ALE ------------------------------------------------------------------
-  # ICE
+  # ICE  -----------------------------------------------------------------------
   feat_effect <- FeatureEffects$new(predictor, method = "ice",
                                     features = names(df_test)[1:(ncol(df_test) - 1)],
-                                    grid.size = 1000)
+                                    grid.size = NUM_GRID_POINTS)
+  results <- lapply(feat_effect$results, function(x) {
+    x$feat_type <- if (is.numeric(x$.borders)) "numeric" else "categorical"
+    x
+  })
   # Combine results
-  res_ice <- rbindlist(feat_effect$results)
-  res_ice <- merge(
-    res_ice, 
+  res <- rbindlist(results)
+  res <- merge(
+    res, 
     data.frame(.id = seq_len(nrow(df_test)), real = df_test[["real"]]), 
     by = ".id")
   
-  # PDP
-  res_pdp <- res_ice[, .(.value = mean(.value)), by = c(".borders", ".type", ".feature")]
+  # ICE results
+  top_idx <- preds$id[seq_len(num_samples_ice %/% 3)]
+  mid_idx <- preds$id[seq(num_samples %/% 2 - num_samples_ice %/% 6, 
+                          num_samples %/% 2 + num_samples_ice %/% 6)]
+  low_idx <- preds$id[seq(num_samples - num_samples_ice %/% 3 + 1, num_samples)]
+  idx <- c(top_idx, mid_idx, low_idx)
+  
+  res_ice <- res[.id %in% idx, ]
+  
+  # PDP ------------------------------------------------------------------------
+  res_pdp <- rbind(
+    cbind(res[, .(.value = mean(.value)), 
+                  by = c(".borders", ".type", ".feature", "feat_type")], real = "both"),
+    cbind(res[real == "Real", .(.value = mean(.value)), 
+                  by = c(".borders", ".type", ".feature", "feat_type")], real = "Real"),
+    cbind(res[real != "Real", .(.value = mean(.value)), 
+                  by = c(".borders", ".type", ".feature", "feat_type")], real = "Synthetic")
+  )
   res_pdp$`.id` <- NA
-  res_pdp$real <- NA
   res_pdp$`.type` <- "pdp"
   
   
-  # ALE
+  # ALE ------------------------------------------------------------------------
   feat_effect <- FeatureEffects$new(predictor, method = "ale",
                                     features = names(df_test)[1:(ncol(df_test) - 1)],
-                                    grid.size = 1000)
-  res_ale <- rbindlist(feat_effect$results)
+                                    grid.size = NUM_GRID_POINTS)
+  results <- lapply(feat_effect$results, function(x) {
+    x$feat_type <- if (is.numeric(x$.value)) "numeric" else "categorical"
+    x
+  })
+  res_ale <- rbindlist(results)
   res_ale$`.id` <- NA
   res_ale$real <- NA
   
+  # ----------------------------------------------------------------------------
   # Summarize results
   res <- rbind(res_ice, res_ale, res_pdp)
   setnames(res, c(".borders", ".value", ".type", ".id", ".feature"),
            c("gridpoint", "value", "method", "id", "feature"))
-  # ----------------------------------------------------------------------------
   
   # Summarize and return results
-  cbind(res,
+  suppressWarnings({
+    df_rug <- 
+      cbind(
+        melt(data.table(df_test),
+             measure.vars = names(df_test)[-ncol(df_test)], id.vars = "real"),
         dataset_name = df$dataset_name[i],
         syn_name = df$syn_name[i],
         run_model = df$run_model[i],
-        model_name = df$model_name[i])
+        model_name = df$model_name[i]
+      )
+  })
+  
+  list(
+    cbind(res,
+        dataset_name = df$dataset_name[i],
+        syn_name = df$syn_name[i],
+        run_model = df$run_model[i],
+        model_name = df$model_name[i]),
+    df_rug
+  )
 })
 
 # Combine results
-res <- do.call(rbind, res)
+rugs <- do.call(rbind, lapply(res, `[[`, 2))
+res <- do.call(rbind, lapply(res, `[[`, 1))
 
 # Save results
 cli_progress_step("Saving results")
 if (!dir.exists(paste0("./results/Q2"))) dir.create(paste0("./results/Q2"), recursive = TRUE)
 saveRDS(res, "./results/Q2/feat_effects.rds")
+saveRDS(rugs, "./results/Q2/feat_effects_rugs.rds")
 
 cli_progress_done("Done!")
